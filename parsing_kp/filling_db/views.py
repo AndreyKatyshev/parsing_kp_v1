74595b89dc96a5a3275eb5e1ec6f766c46b0a43b
Андрей Катышев <Andrey-Katyshev@mail.ru>
2022-11-25 11:06:01 +0300

    add view seve_movie

--- parsing_kp/filling_db/views.py
+++ parsing_kp/filling_db/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+import os
+import requests
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from dotenv import load_dotenv
+
+
+
+def seve_movie(request, movie_id):
+    load_dotenv()
+    open_token = 'ZQQ8GMN-TN54SGK-NB3MKEC-ZKB8V06'
+    my_token = os.getenv('MY_TOKEN')
+    id = movie_id
+    base_url = 'https://api.kinopoisk.dev/movie'
+    url_movie_kp = f'{base_url}?token={open_token}&search={id}&field=id'
+    response = requests.get(url_movie_kp).json()
+    return JsonResponse(response)
--- parsing_kp/parsing_kp/urls.py
+++ parsing_kp/parsing_kp/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from filling_db import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('save_movie/<int:movie_id>/', views.seve_movie, name = 'seve_movie')
 ]
